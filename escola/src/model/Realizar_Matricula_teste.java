a8211c74ebeba804483d58a6eb6e308333012066
Felipe Malengo <felipemalengo1@hotmail.com>
2023-06-16 21:32:12 -0300

    criacao classe de testes

--- escola/src/model/Realizar_Matricula_teste.java
+++ escola/src/model/Realizar_Matricula_teste.java
@@ -12,84 +12,70 @@ import org.junit.jupiter.api.Test;
 
 class Realizar_Matricula_teste {
 
-	@Test
-	void test() {
-
-		
-		// TESTE NAS CLASSES ENVOLVIDAS 
-		
-		Aluno a1 = new Aluno();
-		Aluno a2 = new Aluno("felipe","BP3019047","Ads","41332042805","matriculado");
-        a2.setIdade(2);
-        
-
-        // TESTE DE APRESENTAÇÃO DAS DISCIPLINAS , ACESSANDO O METODO DA CLASSE DISCIPLINA 
-        
-        //LISTAGEM DAS TURMAS DE MODO RESUMIDO
+    @Test
+    void testListarTurmas() {
+        // Teste para listar as turmas disponíveis
         Turma t = new Turma();
         List<String> options = t.listarTurmas();
-        System.out.println( t.listarTurmas());
-	    ArrayList<List> materiasEscolhidas = new  ArrayList<List>();
-        materiasEscolhidas.add(Arrays.asList(options.get(0),options.get(3)));
-        System.out.println("MATERIAS ESCOLHIDAS: "+materiasEscolhidas);
+        Assert.assertFalse(options.isEmpty()); // Verifica se a lista de turmas não está vazia
+        System.out.println("Opções de turmas: " + options);
+    }
 
-        //TESTE PARA VERIFICAR SE A LISTA DE OPÇÕES DE TURMAS NÃO ESTÁ VAZIA:
-        Assert.assertFalse(options.isEmpty());
+    @Test
+    void testEscolherTurmas() {
+        // Teste para escolher as turmas
+        Turma t = new Turma();
+        List<String> options = t.listarTurmas();
+        ArrayList<List> materiasEscolhidas = new ArrayList<List>();
+        materiasEscolhidas.add(Arrays.asList(options.get(0), options.get(3))); // Adiciona turmas escolhidas à lista
+        System.out.println("Matérias escolhidas: " + materiasEscolhidas);
+        Assert.assertFalse(materiasEscolhidas.isEmpty()); // Verifica se a lista de turmas escolhidas não está vazia
+    }
 
-        
-	    Disciplina d1 = new Disciplina("Matemática","MT","Matemática básica",true);
-	    Disciplina d2 = new Disciplina("Portugues","pt", "Portugues básico",true);
-	    Disciplina d3 = new Disciplina("Biologia","BIO", "Biologia médio",true);
-	    Disciplina d4 = new Disciplina("Filosofia","FIL", "Filosofia Médio",true);
-	    ArrayList<Disciplina> lista = new  ArrayList<Disciplina>();
+    @Test
+    void testQuantidadeAtributosTurma() {
+        // Teste para verificar a quantidade de atributos da classe Turma
+        Turma T1 = new Turma("Juliano", 2, 2022, 25, "SALA 5", "DIURNO");
+        Field[] campos = T1.getClass().getDeclaredFields();
+        int quantidadeAtributos = campos.length;
+        Assert.assertEquals(6, quantidadeAtributos); // Verifica se a quantidade de atributos é igual a 6
+    }
 
-	    lista.addAll(Arrays.asList(d1, d2, d3, d4));
+    @Test
+    void testListarDisciplinas() {
+        // Teste para listar as disciplinas disponíveis
+        Disciplina d1 = new Disciplina("Matemática", "MT", "Matemática básica", true);
+        Disciplina d2 = new Disciplina("Portugues", "pt", "Portugues básico", true);
+        Disciplina d3 = new Disciplina("Biologia", "BIO", "Biologia médio", true);
+        Disciplina d4 = new Disciplina("Filosofia", "FIL", "Filosofia Médio", true);
+        ArrayList<Disciplina> lista = new ArrayList<Disciplina>();
+        lista.addAll(Arrays.asList(d1, d2, d3, d4));
+        Assert.assertEquals(4, lista.size()); // Verifica se a lista de disciplinas tem tamanho igual a 4
+    }
 
+    @Test
+    void testQuantidadeAtributosDisciplina() {
+        // Teste para verificar a quantidade de atributos da classe Disciplina
+        Disciplina d1 = new Disciplina("Matemática", "MT", "Matemática básica", true);
+        Field[] campos = d1.getClass().getDeclaredFields();
+        int quantidadeAtributos = campos.length;
+        Assert.assertEquals(4, quantidadeAtributos); // Verifica se a quantidade de atributos é igual a 4
+    }
 
-	    int tamanhoLista = lista.size();
-	    Assert.assertEquals(4, tamanhoLista);
-
-	    
-        //TESTAR A QUANTIDADE DE ATRIBUTOS VINDAS DE UM OBJETO DA CLASSE DISCIPLINA
-        
-        for (Disciplina disciplina : lista) {
-            Field[] campos = disciplina.getClass().getDeclaredFields();
-            int quantidadeAtributos = campos.length;
-            Assert.assertEquals(4, quantidadeAtributos);
-        }
-	    
-	    //TESTE PARA VERIFICAR A ORDEM DAS DISCIPLINAS NA LISTA 
-	    
-	    Assert.assertEquals(d1, lista.get(0));
-	    Assert.assertEquals(d2, lista.get(1));
-	    Assert.assertEquals(d3, lista.get(2));
-	    Assert.assertEquals(d4, lista.get(3));
-
-	    //TESTE PARA VERIFICAR SE A LISTA NÃO CONTÉM UMA DISCIPLINA ESPECÍFICA
-        Assert.assertTrue(lista.contains(d1));
-
+    @Test
+    void testOrdemDisciplinas() {
+        // Teste para verificar a ordem das disciplinas na lista
+        Disciplina d1 = new Disciplina("Matemática", "MT", "Matemática básica", true);
+        Disciplina d2 = new Disciplina("Portugues", "pt", "Portugues básico", true);
+        Disciplina d3 = new Disciplina("Biologia", "BIO", "Biologia médio", true);
+        Disciplina d4 = new Disciplina("Filosofia", "FIL", "Filosofia Médio", true);
+        ArrayList<Disciplina> lista = new ArrayList<Disciplina>();
+        lista.addAll(Arrays.asList(d1, d2, d3, d4));
+        Assert.assertEquals(d1, lista.get(0)); // Verifica se a primeira disciplina na lista é d1
+        Assert.assertEquals(d2, lista.get(1)); // Verifica se a segunda disciplina na lista é d2
+        Assert.assertEquals(d3, lista.get(2)); // Verifica se a terceira disciplina na lista é d3
+        Assert.assertEquals(d4, lista.get(3)); // Verifica se a quarta disciplina na lista é d4
+        Assert.assertTrue(lista.contains(d1)); // Verifica se a lista contém a disciplina d1
         System.out.println(lista);
-	    
-        // TESTE DE APRESENTAÇÃO DAS TURMAS ,  ACESSANDO O METODO DA CLASSE TURMA
- 
-        Turma T1 = new Turma("Juliano",2, 2022, 25, "SALA 5", "DIURNO");
-        Turma T2 = new Turma("Maria",1, 2022, 30, "SALA 7", "NOTURNO");
-        Turma T3 = new Turma("João",1, 2022, 28, "SALA 3", "DIURNO");
-        Turma T4 = new Turma("Ana",1, 2022, 22, "SALA 2", "NOTURNO");
-        ArrayList<Turma> listaTurmas = new  ArrayList<Turma>();
-        listaTurmas.addAll(Arrays.asList(T1, T2, T3, T4));
-
-        //TESTAR A QUANTIDADE DE ATRIBUTOS VINDAS DE UM OBJETO DA CLASSE TURMA
-        
-        for (Turma turma : listaTurmas) {
-            Field[] campos = turma.getClass().getDeclaredFields();
-            int quantidadeAtributos = campos.length;
-            Assert.assertEquals(6, quantidadeAtributos);
-        }
-        
-      
-	    System.out.println(listaTurmas);
-   
-	}
-
+    }
 }
